757a3e17ee2a5683ff2880771516a6e11edf8cf9
VirtuosicAI <39039445+VirtuosicAI@users.noreply.github.com>
2020-06-04 18:35:25 -0500

    Removed unnecessary usings

--- MIDI Merger Lite/MainForm.cs
+++ MIDI Merger Lite/MainForm.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Net.Mail;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using MIDI_Merger_Lite.Properties;
 
